ffe8dbc156c148f420ab1157901a0907c1884939
jonytalukdar <jonytalukdar30@gmail.com>
2021-11-01 11:39:38 +0600

    added suffle card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
+const cardImages = [
+  { src: '/img/helmet-1.png' },
+  { src: '/img/potion-1.png' },
+  { src: '/img/ring-1.png' },
+  { src: '/img/scroll-1.png' },
+  { src: '/img/shield-1.png' },
+  { src: '/img/sword-1.png' },
+];
 
 function App() {
+  //states
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  //suffle cards function
+  const suffleCards = () => {
+    const suffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(suffledCards);
+    setTurns(0);
+  };
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={() => suffleCards()}>New Game</button>
     </div>
   );
 }
 
-export default App
+export default App;
